69b304995f709a765349b7938a1ea7cc446a665b
kmakieiev <kyryl.makieiev@nure.ua>
2024-06-14 11:35:01 +0200

    Initial Commit

---
+++ RepoViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepoViewer
+//
+//  Created by Kyryl Makieiev on 14.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepoViewer/RepoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoViewerApp.swift
+//  RepoViewer
+//
+//  Created by Kyryl Makieiev on 14.06.24.
+//
+
+import SwiftUI
+
+@main
+struct RepoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
